77ed18acded9e6668afae0e7a63e2e63ed2bee11
lewisian8787 <lewisian8787@gmail.com>
2026-05-18 15:40:59 -0400

    initial grafana scraper file created

--- smart_metrics/src/grafanaApiInterface.ts
+++ smart_metrics/src/grafanaApiInterface.ts
@@ -3,18 +3,60 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const GRAFANA_URL = process.env.GRAFANA_URL || 'http://grafana:3000';
+const GRAFANA_URL = process.env.GRAFANA_URL || 'http://localhost:3000';
 const GRAFANA_USER = process.env.GRAFANA_USER || 'admin';
 const GRAFANA_PASSWORD = process.env.GRAFANA_PASSWORD || 'admin';
 
-// export interface {
+// Root response
+export interface QueryHistoryResponse {
+  result: QueryHistoryResult;
+}
 
-// }
+export interface QueryHistoryResult {
+  totalCount: number;
+  queryHistory: QueryHistoryEntry[];
+  page: number;
+  perPage: number;
+}
+
+// Query history entry
+export interface QueryHistoryEntry {
+  uid: string;
+  datasourceUid: string;
+  createdBy: number;
+  createdAt: number;
+  comment: string;
+  queries: QueryDefinition[];
+  starred: boolean;
+}
+
+// Query definition
+export interface QueryDefinition {
+  datasource: DatasourceRef;
+
+  // Shared query fields
+  refId: string;
+
+  // Optional Grafana/VictoriaMetrics fields
+  editorMode?: string;
+  expr?: string;
+  format?: string;
+  instant?: boolean;
+  key?: string;
+  legendFormat?: string;
+  range?: boolean;
+  queryType?: string;
+}
+
+// Datasource reference
+export interface DatasourceRef {
+  type: string;
+  uid: string;
+}
 
 async function collectQueries() {
-  console.log('Polling Grafana query history...');
-
   try {
+    //returns expression at index 0, property expr.
     const response = await axios.get(`${GRAFANA_URL}/api/query-history`, {
       auth: {
         username: GRAFANA_USER,
@@ -22,12 +64,10 @@ async function collectQueries() {
       },
     });
 
-    const queries = response.data.result.queryHistory;
-    console.log(`Found ${queries.length} queries`);
-    }
-
-    console.log('Done inserting queries');
+    const queries = response.data.result.queryHistory
   } catch (err) {
-    console.error('Error collecting queries:', err.message);
+    if (err instanceof Error) console.error('Error', err.message)
   }
 }
+
+
